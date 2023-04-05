86267eb919408ad0cf34768042791edb17dccd54
datastring <little.iron1021@fastmail.com>
2023-04-05 16:05:49 -0500

    happy website noices

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-function showDarkbox(url) {
-    if (!darkBoxVisible) {
-        let x = (window.innerWidth - 1280) / 2;
-        let y = window.scrollY + 50;
-
-        // Create the darkBox
-        var div = document.createElement("div");
-        div.id = "darkbox";
-        div.innerHTML = '<img class="darkboximg" src="' + url + '" />';
-        document.body.appendChild(div);
-        let box = document.getElementById("darkbox");
-        box.style.left = x.toString() + "px";
-        box.style.top = y.toString() + "px";
-        box.style.height = 'auto';
-        box.addEventListener('click', (event) => {
-            // Remove it
-            let element = document.getElementById("darkbox");
-            element.parentNode.removeChild(element);
-
-            darkBoxVisible = false;
-        })
-
-        darkBoxVisible = true;
-
-    } else {
-        // Remove it
-        let element = document.getElementById("darkbox");
-        element.parentNode.removeChild(element);
-
-        darkBoxVisible = false;
-    }
-}
+let darkBoxVisible = false;
 
 window.addEventListener('load', (event) => {
-    let images = document.querySelectorAll("img");
+    let images = document.querySelectorAll('img');
     if (images !== null && images !== undefined && images.length > 0) {
         images.forEach(function (img) {
             img.addEventListener('click', (evt) => {
@@ -41,3 +10,35 @@ window.addEventListener('load', (event) => {
         });
     }
 });
+
+function showDarkbox(url) {
+    if (!darkBoxVisible) {
+        let x = (window.innerWidth - 1280) / 2;
+        let y = window.scrollY + 50;
+
+        // Create the darkBox
+        var div = document.createElement('div');
+        div.id = 'darkbox';
+        div.innerHTML = '<img class="darkboximg" src="' + url + '" />';
+        document.body.appendChild(div);
+        let box = document.getElementById('darkbox');
+        box.style.left = x.toString() + 'px';
+        box.style.top = y.toString() + 'px';
+        box.style.height = 'auto';
+        box.addEventListener('click', (event) => {
+            // Remove it
+            let element = document.getElementById('darkbox');
+            element.parentNode.removeChild(element);
+
+            darkBoxVisible = false;
+        });
+
+        darkBoxVisible = true;
+    } else {
+        // Remove it
+        let element = document.getElementById('darkbox');
+        element.parentNode.removeChild(element);
+
+        darkBoxVisible = false;
+    }
+}
